a28230e2ad8175b0bc2b04d8ea49818408b9d4e3
Josua Geiger <josua.geiger@sap.com>
2024-02-15 07:42:29 +0100

    wip

---
+++ default.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      packages = forAllSystems (system:{
+          app-autoscaler-cli-plugin = nixpkgsFor.${system}.buildGoModule {
+            pname = "app-autoscaler-cli-plugin";
+            version = "latest";
+            src = ./.;
+          };
+      });
+    };
+}
